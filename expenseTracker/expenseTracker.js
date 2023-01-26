26cca16fc01cde56f5bab0878466a074ee2929d9
Brian Bawden <brianbawden001@gmail.com>
2023-01-26 15:23:40 -0500

    working on newExpense function.

--- expenseTracker/expenseTracker.js
+++ expenseTracker/expenseTracker.js
@@ -1,1 +1,9 @@
-console.log("connected")
+
+function newExpense() {
+    console.log("newExpense clicked")
+    const table = document.getElementById("table")
+    const tr = document.createElement("tr")
+    const td = document.createElement("td")
+
+    let date = document.getElementById("getDate")
+}
