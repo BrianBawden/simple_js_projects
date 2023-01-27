669c7c31e16d21e4798ee4d48ddf699e1751a39d
Brian Bawden <brianbawden001@gmail.com>
2023-01-26 20:19:24 -0500

    program will add new expenses to table.

--- expenseTracker/expenseTracker.js
+++ expenseTracker/expenseTracker.js
@@ -1,28 +1,16 @@
 
+const table = document.getElementById("table")
+
+
 function newExpense() {
-    console.log("newExpense clicked")
-    const table = document.getElementById("table")
-    const tr = document.createElement("tr")
-    const td = document.createElement("td")
-
     let date = document.getElementById("getDate").value
-    let createDate = document.createTextNode(date)
     let description = document.getElementById("getDescription").value
-    let createDescription = document.createTextNode(description)
     let amount = document.getElementById("getAmount").value
-    let createAmount = document.createTextNode(amount)
-    
-    console.log(date, description, amount)
 
-    td.appendChild(createDescription)
-    td.appendChild(createDate)
-    td.appendChild(createAmount)
-    tr.appendChild(td)
-    table.appendChild(tr)
-    // td.appendChild(createDescription)
-    // tr.appendChild(td)
-    // table.appendChild(tr) 
-    // td.appendChild(createAmount)
-    // tr.appendChild(td)
-    // table.appendChild(tr)
+    let template = '<tr> <td>' + date + '</td> <td>' + description + '</td> <td>' + '$' + amount + '</td> </tr> ';
+    table.innerHTML += template;
+    
+    document.getElementById("getDate").value =''
+    document.getElementById("getDescription").value =''
+    document.getElementById("getAmount").value =''
 }
