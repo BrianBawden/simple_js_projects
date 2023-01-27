deb1c3e7324003aa7b02ff884b2f1f3c5822fa75
Brian Bawden <brianbawden001@gmail.com>
2023-01-27 13:57:23 -0500

    js prints value of pw length to consol when btn clicked.

---
+++ password_generator/password.js
@@ -0,0 +1,10 @@
+
+
+
+function run(){
+    // let pwLength = document.getElementById("passLength").value
+    let pwLength =document.querySelector("#passLength");
+    pwLOutput = pwLength.value;
+    console.log(pwLOutput);
+
+};
