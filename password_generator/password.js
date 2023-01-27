e9e85d26a70a3e106403f29c082641a0cf9bfd06
Brian Bawden <brianbawden001@gmail.com>
2023-01-27 14:54:08 -0500

    add functions for input and return of input.

--- password_generator/password.js
+++ password_generator/password.js
@@ -2,18 +2,37 @@
 
 
 function run(){
+
+    let leng = runLen()
+    console.log(leng)
+
+    let up = runUp()
+    console.log(up)
+
+    let spe = runSpe()
+    console.log(spe)
+
+};
+
+function runLen(){
     // get length for password
     let pwLength =document.querySelector("#passLength");
     let pwLOutput = pwLength.value;
     
+    return pwLOutput
     // console.log(pwLOutput)
+}
 
+function runUp(){
     // check for upper case
     let upper = document.querySelector('input[name="upper"]:checked').value
     // console.log(upper)
+    return upper
+}
 
+function runSpe() {
     // check for special character
     let specialCh = document.querySelector("input[name='spclC']:checked").value
     // console.log(specialCh)
-
-};
+    return specialCh
+}
