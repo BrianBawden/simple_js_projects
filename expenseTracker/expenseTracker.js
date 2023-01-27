e3da73cea2a27e4083e2593ce50f5d963b0a6e56
Brian Bawden <brianbawden001@gmail.com>
2023-01-26 19:22:29 -0500

    working on adding information to table

--- expenseTracker/expenseTracker.js
+++ expenseTracker/expenseTracker.js
@@ -5,8 +5,24 @@ function newExpense() {
     const tr = document.createElement("tr")
     const td = document.createElement("td")
 
-    let date = document.getElementById("getDate")
-    let description = document.getElementById("getDescription")
-    let amount = document.getElementById("getAmount")
+    let date = document.getElementById("getDate").value
+    let createDate = document.createTextNode(date)
+    let description = document.getElementById("getDescription").value
+    let createDescription = document.createTextNode(description)
+    let amount = document.getElementById("getAmount").value
+    let createAmount = document.createTextNode(amount)
+    
+    console.log(date, description, amount)
 
+    td.appendChild(createDescription)
+    td.appendChild(createDate)
+    td.appendChild(createAmount)
+    tr.appendChild(td)
+    table.appendChild(tr)
+    // td.appendChild(createDescription)
+    // tr.appendChild(td)
+    // table.appendChild(tr) 
+    // td.appendChild(createAmount)
+    // tr.appendChild(td)
+    // table.appendChild(tr)
 }
